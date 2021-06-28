755605d0e7e61653faa1e49d7a760f24be41da8c
SoyNegro <krisoul.valdes@gmail.com>
2021-06-28 17:17:40 -0400

    added classification service

--- src/service/ClassificationService.kt
+++ src/service/ClassificationService.kt
@@ -1,2 +1,26 @@
 package com.thedarksideofcode.service
 
+import com.thedarksideofcode.DB
+import com.thedarksideofcode.models.Classification
+import org.litote.kmongo.eq
+
+class ClassificationService {
+
+  suspend fun getClassificationById(id:String): Classification? {
+      return DB.classificationCollection.findOne(Classification::classificationID eq id)
+  }
+
+  suspend fun saveClassification(classification: Classification){
+      DB.classificationCollection.save(classification)
+  }
+
+  suspend fun updateClassificationById(classification: Classification){
+      DB.classificationCollection.updateOne(Classification::classificationID eq classification.classificationID,classification)
+  }
+
+  suspend fun deleteClassificationById(id: String){
+      DB.classificationCollection.deleteOne(Classification::classificationID eq id)
+  }
+
+
+}
