c92748d00eef92a66a49906a1d9da8065f585dcc
SoyNegro <krisoul.valdes@gmail.com>
2021-06-28 22:59:51 -0400

    updated user model

--- src/models/User.kt
+++ src/models/User.kt
@@ -1,13 +1,19 @@
 package com.thedarksideofcode.models
 
+import java.time.LocalDateTime
+
 data class UserConfiguration(
-    val canSeeStrongContent:Boolean,
-    val canSeeNFSWContent: Boolean,
+    val allowSeeStrongContent:Boolean,
+    val allowNFSWContent: Boolean,
 
 )
 
+//TODO("Bring bookmarks out to play")
 data class User(
+    val userName:String?,
     val email:String,
     val password:String,
-    val role:String,
+    val role:String?,
+    val bookmarks:MutableList<Bookmark>?,
+    var lastConnection:LocalDateTime = LocalDateTime.now()
 )
