2a394577f91f645da1e1eb48ae3ce5dc0e6208e1
SoyNegro <krisoul.valdes@gmail.com>
2021-06-28 23:02:38 -0400

    updated services

--- src/service/ServicesDI.kt
+++ src/service/ServicesDI.kt
@@ -4,6 +4,9 @@ import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.singleton
 
-fun DI.MainBuilder.bindService(){
-bind<BookService>() with singleton { BookService() }
+fun DI.MainBuilder.bindService() {
+    bind<BookService>() with singleton { BookService() }
+    bind<CatalogService>() with singleton { CatalogService() }
+    bind<ClassificationService>() with singleton { ClassificationService() }
+    bind<UserService>() with singleton { UserService() }
 }
