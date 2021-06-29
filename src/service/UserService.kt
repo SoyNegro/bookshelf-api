7f323ddb82d164f7d18d50a5b5e11aa55082069f
SoyNegro <krisoul.valdes@gmail.com>
2021-06-28 23:02:13 -0400

    basic user service

--- src/service/UserService.kt
+++ src/service/UserService.kt
@@ -1,2 +1,23 @@
 package com.thedarksideofcode.service
 
+import com.thedarksideofcode.DB
+import com.thedarksideofcode.models.User
+import org.litote.kmongo.eq
+
+class UserService {
+    suspend fun getUserByUsername(username:String): User? {
+        return DB.userCollection.findOne(User::userName eq username)
+    }
+
+    suspend fun saveUser(user: User){
+        DB.userCollection.insertOne(user)
+    }
+
+    suspend fun updateUserByUsername(user: User){
+        DB.userCollection.updateOne(User::userName eq user.userName,user)
+    }
+
+    suspend fun deleteUserByUsername(username:String){
+        DB.userCollection.deleteOne(User::userName eq username )
+    }
+}
