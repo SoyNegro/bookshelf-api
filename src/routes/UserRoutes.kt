06fb95b35286fa8d4a62957c30290b4df7c66ba7
SoyNegro <krisoul.valdes@gmail.com>
2021-06-28 23:00:30 -0400

    user routes draft

--- src/routes/UserRoutes.kt
+++ src/routes/UserRoutes.kt
@@ -1,3 +1,24 @@
 package com.thedarksideofcode.routes
 
-// TODO("Get user routes done")
+import com.thedarksideofcode.service.UserService
+import io.ktor.routing.*
+import org.kodein.di.instance
+import org.kodein.di.ktor.di
+
+fun Route.userRouting(){
+    val userService by di().instance<UserService>()
+    route("/user/"){
+        get("{username}") {
+
+        }
+        post {
+
+        }
+        put {
+
+        }
+        delete("{username}") {
+
+        }
+    }
+}
